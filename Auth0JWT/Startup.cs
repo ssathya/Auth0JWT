911fca4de09afe75a263abf3ceea3faa6b72753b
Sridhar Sathya <sridhar_0314@live.com>
2021-04-05 08:08:04 -0400

    fixed #1
    Created Quiz service

--- Auth0JWT/Startup.cs
+++ Auth0JWT/Startup.cs
@@ -29,6 +29,8 @@ namespace Auth0JWT
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
 			services.AddSingleton<WeatherForecastService>();
+
+			services.AddSingleton<QuizService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
